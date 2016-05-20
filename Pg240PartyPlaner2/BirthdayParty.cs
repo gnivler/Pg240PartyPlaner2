c57005f4ceea832ea5ae707a470ea8c60321024b
gnivler <gnivil@gmail.com>
2016-05-20 17:53:07 -0600

    Add project files.

---
+++ Pg240PartyPlaner2/BirthdayParty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg240PartyPlaner2 {
+    class BirthdayParty {
+        public const int CostOfFoodPerPerson = 25;
+        public int NumberOfGuests { get; set; }
+        public bool FancyDecorations { get; set; }
+        public string CakeWriting { get; set; }
+
+        public BirthdayParty(int numberOfGuests, bool fancyDecorations, string cakeWriting) {
+            NumberOfGuests = NumberOfGuests;
+            FancyDecorations = fancyDecorations;
+            CakeWriting = cakeWriting;
+        }
+
+
+    }
+}
